6676b5c559f3506b9062289d0bf755075a6db7dd
sepopur0 <donghuongmen@gmail.com>
2022-11-15 21:31:46 +0700

    first commit

---
+++ Client/script.py
@@ -0,0 +1,17 @@
+from Client import Client
+import socket
+import UI
+import GUII
+
+#client = Client.Client()
+#ui = UI.UI(client)
+#ui.Run()
+
+client = Client()
+client.run()
+#
+#gui.run()
+
+#
+#gui.run()
+
